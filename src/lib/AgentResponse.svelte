4af113d89717f68ee87569688e7bc011c73ce8da
dang.nguyen <dang.nguyen@vnsilicon.net>
2025-04-03 12:58:08 +0700

    implement ui

--- src/lib/AgentResponse.svelte
+++ src/lib/AgentResponse.svelte
@@ -8,7 +8,10 @@
     alt="agent logo"
   />
 
-  <div class="space-y-3 overflow-x-auto overflow-y-hidden">
+  <div
+    class="space-y-3 overflow-x-auto overflow-y-hidden"
+    style="width: 1100px;"
+  >
     <slot />
   </div>
 </li>
